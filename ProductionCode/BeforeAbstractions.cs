19dabe1d8331eca63b2e34f740b30df677fa9097
Sara Gowen <sara.gowen@ukho.gov.uk>
2022-11-25 14:26:56 +0000

    Added untestable code

---
+++ ProductionCode/BeforeAbstractions.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ProductionCode;
+
+[ExcludeFromCodeCoverage] //Interacting with the actual file system is untestable, this is why we need to use System.IO.Abstractions
+public class BeforeAbstractions
+{
+    public void ReplaceAFile(string filePath)
+    {
+        var fileContents = File.ReadAllLines(filePath);
+
+        var count = 1;
+        var newFileContents = new List<string>();
+        foreach (var line in fileContents)
+        {
+            newFileContents.Add($"{line} altered {count}");
+            count++;
+        }
+
+        File.Delete(filePath);
+        File.WriteAllLines(filePath, newFileContents);
+    }
+
+    public void AlterTheMiddleOfAFile(string filePath)
+    {
+        using var stream = File.Open(filePath, FileMode.Open);
+        var midpoint = stream.Length / 2;
+        stream.Seek(midpoint, SeekOrigin.Begin);
+
+        using var sw = new StreamWriter(stream);
+        sw.Write("Middle");
+        
+        sw.Flush();
+        sw.Close();
+    }
+    public async Task AlterTheMiddleOfAFileAsync(string filePath)
+    {
+        await using var stream = File.Open(filePath, FileMode.Open);
+        var midpoint = stream.Length / 2;
+        stream.Seek(midpoint, SeekOrigin.Begin);
+
+        await using var sw = new StreamWriter(stream);
+        await sw.WriteAsync("Middle");
+        
+        await sw.FlushAsync();
+        sw.Close();
+    }
+
+
+    public async Task AlterTheMiddleOfManyFiles(string directoryPath)
+    {
+        var files = Directory.GetFiles(directoryPath);
+
+        await Task.WhenAll(files.Select(AlterTheMiddleOfAFileAsync));
+    }
+}
---
+++ ProductionCode/Program.cs
@@ -0,0 +1,45 @@
+﻿using ProductionCode;
+
+var beforeAbstractions = new BeforeAbstractions();
+
+Console.WriteLine(
+@" _    _   _____   _      _      _____
+| |  | | |  ___| | |    | |    |  _  |
+| |__| | | |___  | |    | |    | | | |
+|  __  | |  ___| | |    | |    | | | |
+| |  | | | |___  | |___ | |___ | |_| |
+|_|  |_| |_____| |_____||_____||_____|
+");
+
+Console.WriteLine("Gimme a file path");
+var path = Console.ReadLine();
+
+SelectOption:
+Console.WriteLine("What do you want to do to that file?");
+Console.WriteLine("1 - Replace it");
+Console.WriteLine("2 - Alter middle of it");
+Console.WriteLine("3 - It's a directory not a file! Alter middle of all files in the directory");
+
+var option = Console.ReadLine();
+
+switch (option)
+{
+    case "1":
+        beforeAbstractions.ReplaceAFile(path);
+        break;
+    case "2":
+        beforeAbstractions.AlterTheMiddleOfAFile(path);
+        break;
+    case "3":
+        await beforeAbstractions.AlterTheMiddleOfManyFiles(path);
+        break;
+    default:
+        Console.WriteLine("That wasn't an option!");
+        goto SelectOption;
+}
+
+Console.WriteLine("Done");
+Console.WriteLine();
+Console.WriteLine(@"Have a nice day \(o.o)/");
+
+//Note to readers of this code. GoTos are bad, don't use them
